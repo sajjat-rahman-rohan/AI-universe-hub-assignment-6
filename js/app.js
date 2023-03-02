c872a8aa494674670ed03efa785d457db67230ba
mdrohan <mdsajed210@gmail.com>
2023-03-03 02:41:42 +0600

    first card design on html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// const singlePlayer = (id) => {
+//   const URL = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${id}`;
+//   fetch(URL)
+//     .then((res) => res.json())
+//     .then((data) => showSinglePlayer(data.players[0]));
+// };
+
+// api link card load function
+
+// card data show function
+
+loadCard();
