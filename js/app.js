a203ab5fff9bf6cd485a321e6cffaa3b1fae4d55
mdrohan <mdsajed210@gmail.com>
2023-03-03 04:16:44 +0600

    added dynamic card display show img,title,description,date

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const loadCard = (id) => {
   fetch(URL)
     .then((res) => res.json())
     .then((data) => {
-      showCarddata(data.data);
+      showCarddata(data.data.tools);
     });
 };
 
@@ -19,6 +19,57 @@ const loadCard = (id) => {
 const showCarddata = (data) => {
   console.log(data);
   // html data inject with dom
+  const cardContainer = document.getElementById("card-info");
+  cardContainer.innerHTML = "";
+
+  data.forEach((data) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+       <div class="card">
+        <div class="card-body">
+           <img src="${data.image}" class="card-img w-100 " alt="..." />
+           <h5 class="card-title mt-2">Features</h5>
+           <ul>
+              <li>1. ${
+                data.features[0] ? data.features[0] : "No data Found"
+              } </li>
+              <li>2. ${
+                data.features[1] ? data.features[1] : "No data Found"
+              } </li>
+              <li>3. ${
+                data.features[2] ? data.features[2] : "No data Found"
+              } </li>
+           </ul>
+           <hr />
+  
+           <div
+              class="card-text d-flex justify-content-between align-items-center">
+              <div class="card-name-date">
+              <h5 class="card-title">${
+                data.name ? data.name : "No data Found"
+              }</h5>
+              <p class="card-text mb-2"> ${
+                data.description
+                  ? data.description.slice(0, 28)
+                  : "No data Found"
+              }</p>
+              <p class="card-date">
+                  <i class="bi bi-calendar4-week"></i>   ${
+                    data.published_in ? data.published_in : "No data Found"
+                  }
+              </p>
+              </div>
+              <button class="card-details">
+              <i class="bi bi-arrow-right"></i>
+              </button>
+           </div>
+        </div>
+       </div>
+    `;
+
+    cardContainer.appendChild(div);
+  });
 };
 
 loadCard();
