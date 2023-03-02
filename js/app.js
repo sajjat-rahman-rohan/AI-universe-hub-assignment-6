318e541d86c8a578935156ef8a017a29a898113b
mdrohan <mdsajed210@gmail.com>
2023-03-03 02:52:31 +0600

    api fecth data show console

--- js/app.js
+++ js/app.js
@@ -6,7 +6,19 @@
 // };
 
 // api link card load function
+const loadCard = (id) => {
+  const URL = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      showCarddata(data.data);
+    });
+};
 
 // card data show function
+const showCarddata = (data) => {
+  console.log(data);
+  // html data inject with dom
+};
 
 loadCard();
