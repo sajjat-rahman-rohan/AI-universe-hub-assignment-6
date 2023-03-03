89831fb627bdbfc7ecef10afa7b8a6de1070f25d
mdrohan <mdsajed210@gmail.com>
2023-03-03 17:11:27 +0600

    show all card , modal with dymanic

--- js/app.js
+++ js/app.js
@@ -97,13 +97,17 @@ const loadCardDetails = (id) => {
     .then((data) => displayCardDetails(data.data));
 };
 
+// display
 const displayCardDetails = (data) => {
   console.log(data.features.feature_name);
 
+  const featureValue = Object.values(data.features);
+  console.log(featureValue[0].feature_name);
+
   const cardModal = document.getElementById("card-modal");
   cardModal.innerText = "";
-  //   modalTitle.innerText = data.tool_name;
-  //   //   data.forEach((data) => {
+
+  //   data.forEach((data) => {
   const div = document.createElement("div");
   div.classList.add("row");
   div.innerHTML = `
@@ -111,7 +115,7 @@ const displayCardDetails = (data) => {
      <div class="card modal-info">
         <div class="card-body">
         <h5 class="card-title">
-         ${data.description}
+         ${data.description ? data.description : "No data Found"}
         </h5>
 
         <div
@@ -119,17 +123,25 @@ const displayCardDetails = (data) => {
         >
             <div class="modal-price one">
             <h5>
-              ${data.pricing[0].price}   ${data.pricing[0].plan}
+              ${
+                data.pricing[0].price ? data.pricing[0].price : "No data Found"
+              }   ${
+    data.pricing[0].plan ? data.pricing[0].plan : "No data Found"
+  }
             </h5>
             </div>
             <div class="modal-price two">
             <h5>
-            ${data.pricing[1].price}   ${data.pricing[1].plan}
+            ${
+              data.pricing[1].price ? data.pricing[1].price : "No data Found"
+            }   ${data.pricing[1].plan ? data.pricing[1].plan : "No data Found"}
             </h5>
             </div>
             <div class="modal-price three">
             <h5>
-            ${data.pricing[2].price}   ${data.pricing[2].plan}
+            ${
+              data.pricing[2].price ? data.pricing[2].price : "No data Found"
+            }   ${data.pricing[2].plan ? data.pricing[2].plan : "No data Found"}
             </h5>
             </div>
         </div>
@@ -138,14 +150,32 @@ const displayCardDetails = (data) => {
             class="d-flex justify-content-between align-items-center"
         >
             <ul>
-            <li>1. ${data.features}</li>
-            <li>2. Multilingual support</li>
-            <li>3. Seamless integration</li>
+            <li>1. ${
+              featureValue[0].feature_name
+                ? featureValue[0].feature_name
+                : "No data Found"
+            } </li>
+            <li>2. ${
+              featureValue[1].feature_name
+                ? featureValue[1].feature_name
+                : "No data Found"
+            }</li>
+            <li>3. ${
+              featureValue[2].feature_name
+                ? featureValue[2].feature_name
+                : "No data Found"
+            }</li>
             </ul>
             <ul>
-            <li>1. ${data.integrations[0]}</li>
-            <li>2. ${data.integrations[1]}</li>
-            <li>3. ${data.integrations[2]}</li>
+            <li>1. ${
+              data.integrations[0] ? data.integrations[0] : "No data Found"
+            }</li>
+            <li>2. ${
+              data.integrations[1] ? data.integrations[1] : "No data Found"
+            }</li>
+            <li>3. ${
+              data.integrations[2] ? data.integrations[2] : "No data Found"
+            }</li>
             </ul>
         </div>
         </div>
@@ -155,14 +185,22 @@ const displayCardDetails = (data) => {
      <div class="card">
         <div class="card-body">
         <img
-            src="${data.image_link[0]}"
+            src="${data.image_link[0] ? data.image_link[0] : "No Image Found"}"
             class="card-img-top w-100"
             alt="..."
         />
         <div class="card-text-info text-center">
-            <h5 class="card-title">${data.input_output_examples[0].input}</h5>
+            <h5 class="card-title">${
+              data.input_output_examples[0].input
+                ? data.input_output_examples[0].input
+                : "No data Found"
+            }</h5>
             <p class="card-text">
-            ${data.input_output_examples[0].output}
+            ${
+              data.input_output_examples[0].output
+                ? data.input_output_examples[0].output
+                : "No data Found"
+            }
             </p>
         </div>
         </div>
